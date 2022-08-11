9bc106917b43e7ba028e1dcd4e6a466d44f7a187
swapna <swap@mail.com>
2022-08-11 15:20:39 +0530

    commit latest by dev1

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
-class hello 
+class hello  # hello from dev1
 {
-	public static void main ()
+	public static void main () # void from dev1v
 	{
-		Sysytem.out.println ("hello from dev1")
+		Sysytem.out.println ("hello from developer1")
 
 
 	}
